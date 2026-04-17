103631bcc4de8a5a34234c3f169f0e935416a60e
WJH <3506456886@qq.com>
2026-04-17 16:19:02 +0800

    fix: base route

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/utseus-competences.github.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
